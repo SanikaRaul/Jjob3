a4c9d92c8d9c0bb2c315242f57038e7b9ce1eca2
sanika <sanikaraul06@gmail.com>
2021-05-02 12:10:38 +0530

    First Commit

---
+++ jthree.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class jthree {
+
+    public static void main(String[] args) {
+
+        
+     
+        System.out.println("You entered into job3 " );
+    }
+}
